5461837576588285b063f01118b2ad4b8923fb15
John Doe <johndoe@email.com>
2023-02-04 11:00:32 +0530

    contract

---
+++ contracts/Crowdfunding.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Crowdfunding {
+    struct Campaign{
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amount_collected;
+        string image;
+        address[] donators;
+        address[] donations;
+    }
+}
